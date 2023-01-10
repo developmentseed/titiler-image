2db82aa2126f5436e8dd8a3aaa0713d4f74f8195
vincentsarago <vincent.sarago@gmail.com>
2023-01-10 11:31:00 +0100

    enable gcps from file

--- titiler/image/dependencies.py
+++ titiler/image/dependencies.py
@@ -1,9 +1,12 @@
 """titiler-image dependencies."""
 
+import json
 from dataclasses import dataclass
 from enum import Enum
 from typing import List, Optional
 
+import httpx
+from cachetools import TTLCache, cached
 from rasterio.control import GroundControlPoint
 from rasterio.enums import Resampling
 
@@ -36,24 +39,57 @@ class DatasetParams(DefaultDependency):
         self.resampling_method = self.resampling_method.value  # type: ignore
 
 
+@cached(TTLCache(maxsize=512, ttl=3600))
+def get_gcps(gcps_file: str) -> List[GroundControlPoint]:
+    """Fetch and parse GCPS file."""
+    if gcps_file.startswith("http"):
+        body = httpx.get(gcps_file).json()
+    else:
+        with open(gcps_file, "r") as f:
+            body = json.load(f)
+
+    return [
+        # GroundControlPoint(row, col, x, y, z)
+        GroundControlPoint(
+            f["properties"]["y"],
+            f["properties"]["x"],
+            *f["geometry"]["coordinates"],
+            id=f.get("id")
+        )
+        for f in body["features"]
+    ]
+
+
 @dataclass
 class GCPSParams(DefaultDependency):
     """GCPS parameters."""
 
-    gcps: Optional[List[str]] = Query(
-        None,
-        title="Ground Control Points",
-        description="Ground Control Points",
-    )
+    gcps: List[GroundControlPoint] = None
 
-    def __post_init__(self):
-        """Post Init."""
-        if self.gcps:
+    def __init__(
+        self,
+        gcps: Optional[List[str]] = Query(
+            None,
+            title="Ground Control Points",
+            description="Ground Control Points",
+        ),
+        gcps_file: Optional[str] = Query(
+            None,
+            title="Ground Control Points Filepath",
+        ),
+    ):
+        """Initialize GCPSParams
+
+        Note: We only want `gcps` to be forwarded to the reader so we use a custom `__init__` method used by FastAPI to parse the QueryParams.
+        """
+        if gcps:
             self.gcps: List[GroundControlPoint] = [  # type: ignore
-                GroundControlPoint(*list(map(float, gcps.split(","))))
-                for gcps in self.gcps
+                GroundControlPoint(*list(map(float, gcps.split(",")))) for gcps in gcps
             ]
-            if len(self.gcps) < 3:
-                raise HTTPException(
-                    status_code=400, detail="Need at least 3 gcps to wrap an image."
-                )
+        elif gcps_file:
+            self.gcps = get_gcps(gcps_file)
+
+        if self.gcps and len(self.gcps) < 3:
+            raise HTTPException(
+                status_code=400, detail="Need at least 3 gcps to wrap an image."
+            )
